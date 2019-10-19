7295be2b95d9015cf598efd0c3bb64975560a174
Oxy <anoschenko.ov@phystech.edu>
2019-10-19 06:29:32 +0300

    Создала отдельный компонент для сообщения

--- web-components/src/components/MessageForm.js
+++ web-components/src/components/MessageForm.js
@@ -8,70 +8,16 @@ template.innerHTML = `
             width: calc(100% - 2px);
         }
 
-        .result {
-            position: relative;
-            display: flex;
-            flex-direction: column;
-            width: auto;
-            min-width: 30%;
-            max-width: 70%;
-            height: auto;
-            min-height: 85px;
-            font-size: 30px;
-            background-color: #f3e6f5;
-            border: 1px solid #666;
-            margin: 10px 60px 10px 10px;
-            padding: 10px;
-            border-radius: 3px;
-            align-self: flex-end;
-        }
-        
-        .result:before {
-            content: '';
-            position: absolute;
-            width: 0;
-            height: 0;
-            right: -30px;
-            bottom: -1px;
-            border: 15px solid;
-            border-color: transparent transparent #666 #666;
-        }
-        
-        .result:after {
-            content: ' ';
-            position: absolute;
-            width: 0;
-            height: 0;
-            right: -28px;
-            bottom: 0px;
-            border: 15px solid;
-            border-color: transparent transparent #f3e6f5 #f3e6f5;
-        }
-
         .chat_space {
                 display: flex;
-                flex-direction: column;
+                padding-right: 10px;
+                magrin-right: 10px;
+                flex-direction: column-reverse;
+                align-items: flex-end;
                 height: calc(100vh - 260px);
                 overflow-y: auto;
                 will-change: transform;
-        }
 
-        .message {
-            display: flex;
-            align-self: flex-start;
-            font-size: 35px;
-            padding: 15px 10px;
-            color: black;
-            line-height: 1;
-            word-break: break-all;
-        }
-
-        .time {
-            display: flex;
-            align-self: flex-end;
-            font-size: 25px;
-            color: gray;
-            padding-right: 1vh;
         }
 
         input[type=submit] {
@@ -93,7 +39,15 @@ class MessageForm extends HTMLElement {
     this.$form = this.shadowRoot.querySelector('form');
     this.$input = this.shadowRoot.querySelector('form-input');
     this.$chat_space = this.shadowRoot.querySelector('.chat_space');
-    this.recover();
+
+    this.$messageHistory = JSON.parse(localStorage.getItem('key')) || [];
+    for (let i = 0; i < this.$messageHistory.length; i += 1) {
+      const newMessage = document.createElement('my-message');
+      newMessage.innerText = this.$messageHistory[i].innerText;
+      newMessage.time = this.$messageHistory[i].time;
+      this.$chat_space.insertBefore(newMessage, this.$chat_space.firstChild);
+    }
+
     this.$form.addEventListener('submit', this.onSubmit.bind(this));
     this.$form.addEventListener('keypress', this.onKeyPress.bind(this));
   }
@@ -103,38 +57,9 @@ class MessageForm extends HTMLElement {
     if (this.$input.value === '') {
       return;
     }
-    this.message = {};
-    this.message.innerText = this.$input.value;
-    this.message.time = new Date();
-    this.newMessage(this.message);
-    this.$input.value = '';
-    this.addMessageInLocal(this.message);
-  }
-
-  onKeyPress(event) {
-    if (event.keyCode === 13) {
-      this.$form.dispatchEvent(new Event('submit'));
-    }
-  }
-
-  recover() {
-    const messageHistory = JSON.parse(localStorage.getItem('key'));
-    if (messageHistory == null) {
-      return;
-    }
-    for (let i = 0; i < messageHistory.length; i += 1) {
-      this.newMessage(messageHistory[i]);
-    }
-  }
-
-  newMessage(message) {
-    const window = document.createElement('div');
-    window.className = 'result';
-    const text = document.createElement('div');
-    text.className = 'message';
-    text.innerText = message.innerText;
-    const time = document.createElement('div');
-    time.className = 'time';
+    const message = document.createElement('my-message');
+    message.innerText = this.$input.value;
+    message.time = new Date();
     const date = new Date(message.time);
     const min = date.getMinutes();
     const hours = date.getHours();
@@ -146,19 +71,24 @@ class MessageForm extends HTMLElement {
     if (hours < 10) {
       a = '0';
     }
-    time.innerText = `${a}${hours}:${b}${min}`;
-    window.appendChild(text);
-    window.appendChild(time);
-    this.$chat_space.appendChild(window);
+    message.time = `${a}${hours}:${b}${min}`;
+    this.$messageHistory.push({ innerText: message.innerText, time: message.time });
+    this.$chat_space.insertBefore(message, this.$chat_space.firstChild);
+    this.$input.value = '';
+    this.addMessageInLocal();
+  }
+
+  onKeyPress(event) {
+    if (event.keyCode === 13) {
+      this.$form.dispatchEvent(new Event('submit'));
+    }
   }
 
   addMessageInLocal() {
-    let messageHistory = JSON.parse(localStorage.getItem('key'));
-    if (messageHistory == null) {
-      messageHistory = [];
+    if (this.$messageHistory === null) {
+      this.$messageHistory = [];
     }
-    messageHistory.push(this.message);
-    localStorage.setItem('key', JSON.stringify(messageHistory));
+    localStorage.setItem('key', JSON.stringify(this.$messageHistory));
   }
 }
 
