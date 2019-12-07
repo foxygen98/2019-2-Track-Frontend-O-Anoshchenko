f09fb11e93f5f2b55615e152dd0faa337395b272
Oxy <anoschenko.ov@phystech.edu>
2019-12-07 20:38:24 +0300

    Пробую задеплоить дз

---
+++ build/precache-manifest.a602c8811e2d5333221a339491ba7e2a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f321ec154efd939924104164bb4617c1",
+    "url": "/your_repo_name/index.html"
+  },
+  {
+    "revision": "bb2e1a53a7b4c8ba0c9b",
+    "url": "/your_repo_name/static/css/main.7ec178e2.chunk.css"
+  },
+  {
+    "revision": "333cb8d72210953d86fc",
+    "url": "/your_repo_name/static/js/2.300a5db9.chunk.js"
+  },
+  {
+    "revision": "bb2e1a53a7b4c8ba0c9b",
+    "url": "/your_repo_name/static/js/main.36a795b9.chunk.js"
+  },
+  {
+    "revision": "30f1fbcedc29eace620c",
+    "url": "/your_repo_name/static/js/runtime-main.23be6665.js"
+  },
+  {
+    "revision": "3e63fa7f1adaf8cfad51593033def6a6",
+    "url": "/your_repo_name/static/media/attachment.3e63fa7f.svg"
+  },
+  {
+    "revision": "cfd63f108d602a0e84483649f7e44685",
+    "url": "/your_repo_name/static/media/avatar.cfd63f10.svg"
+  },
+  {
+    "revision": "c950727fdd2eafd011a585bf3341eb4b",
+    "url": "/your_repo_name/static/media/menu.c950727f.svg"
+  },
+  {
+    "revision": "b929f75dd636ae1e4371e228d3408472",
+    "url": "/your_repo_name/static/media/pen.b929f75d.svg"
+  },
+  {
+    "revision": "87b90016cd296eff9addeecda11fe526",
+    "url": "/your_repo_name/static/media/return_back.87b90016.svg"
+  },
+  {
+    "revision": "3e44f7ddcf98f465994797433191ec8d",
+    "url": "/your_repo_name/static/media/search.3e44f7dd.svg"
+  },
+  {
+    "revision": "0fee2cb328191e9970b9f101b74cfd4a",
+    "url": "/your_repo_name/static/media/settings.0fee2cb3.svg"
+  },
+  {
+    "revision": "6214c69c5247c7e4c233be60710815f7",
+    "url": "/your_repo_name/static/media/status.6214c69c.svg"
+  }
+]);
