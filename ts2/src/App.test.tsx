01a4c7c1430812c2c14b9a88e43233998304d5d2
Oxy <anoschenko.ov@phystech.edu>
2020-05-03 22:10:30 +0300

    some travis fixes

--- ts2/src/App.test.tsx
+++ ts2/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders results link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText('Результаты экзаменов по математике');
   expect(linkElement).toBeInTheDocument();
 });
