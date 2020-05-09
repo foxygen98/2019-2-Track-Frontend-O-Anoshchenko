923b0beb432e18bb261789cc665fc8455feeaafe
Oxy <anoschenko.ov@phystech.edu>
2020-05-09 03:21:29 +0300

    travis fix

--- exam_2/src/App.test.tsx
+++ exam_2/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders translator link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText('TechnoTrack Translate');
   expect(linkElement).toBeInTheDocument();
 });
