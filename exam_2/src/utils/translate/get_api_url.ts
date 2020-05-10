1b13e0d4e0fb27ffb49b9c4122f0a2254575b18c
foxygen98 <55688132+foxygen98@users.noreply.github.com>
2020-05-10 18:42:56 +0300

    Update exam_2/src/utils/translate/get_api_url.ts
    Co-authored-by: Alexey Opalev <levisolympus@gmail.com>

--- exam_2/src/utils/translate/get_api_url.ts
+++ exam_2/src/utils/translate/get_api_url.ts
@@ -1,7 +1,7 @@
 import {api_key, api_url} from './constants'
 import * as T from './types'
 
-export default function get_api_url (param: T.ITranslate): any {
+export default function get_api_url (param: T.ITranslate): string {
     if (param.format) {
         return encodeURI(`${api_url}?key=${api_key}&text=${param.text}&lang=${param.lang}&format=${param.format}`)
     }
