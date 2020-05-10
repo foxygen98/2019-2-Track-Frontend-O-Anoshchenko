ed0e44109a1d6e135f98d2c25f263821f10569e4
Oxy <anoschenko.ov@phystech.edu>
2020-05-10 20:00:27 +0300

    + git-secret

--- exam_2/src/utils/translate/constants.ts
+++
@@ -1,2 +0,0 @@
-export const api_key: string = 'trnsl.1.1.20200416T185913Z.02ec96c0555185bf.e2116c8ca8c9d634d927b802fa065cf2c9349273'
-export const api_url: string = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
