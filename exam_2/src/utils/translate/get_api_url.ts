1478ba0ac7e6777d9ad7f9920c74456fbfef77ff
Oxy <anoschenko.ov@phystech.edu>
2020-05-11 14:20:07 +0300

    ...

--- exam_2/src/utils/translate/get_api_url.ts
+++ exam_2/src/utils/translate/get_api_url.ts
@@ -1,9 +1,9 @@
-import {api_key, api_url} from './constants'
+// import {api_key, api_url} from './constants'
 import * as T from './types'
 
 export default function get_api_url (param: T.ITranslate): string {
     if (param.format) {
-        return encodeURI(`${api_url}?key=${api_key}&text=${param.text}&lang=${param.lang}&format=${param.format}`)
+        return encodeURI(`${process.env.api_url}?key=${process.env.api_key}&text=${param.text}&lang=${param.lang}&format=${param.format}`)
     }
-    return encodeURI(`${api_url}?key=${api_key}&text=${param.text}&lang=${param.lang}`)
+    return encodeURI(`${process.env.api_url}?key=${process.env.api_key}&text=${param.text}&lang=${param.lang}`)
 }
