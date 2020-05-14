22fe8b317958b25bb5ebcdfea965fd1f21693829
Oxy <anoschenko.ov@phystech.edu>
2020-05-14 20:24:22 +0300

    вернула ключи

--- exam_2/src/utils/translate/get_api_url.ts
+++ exam_2/src/utils/translate/get_api_url.ts
@@ -1,11 +1,9 @@
-// import {api_key} from './constants'
+import {api_key, api_url} from './constants'
 import * as T from './types'
-
-const api_url: string = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
 export default function get_api_url (param: T.ITranslate): string {
     if (param.format) {
-        return encodeURI(`${api_url}?key=${process.env.API_KEY}&text=${param.text}&lang=${param.lang}&format=${param.format}`)
+        return encodeURI(`${api_url}?key=${api_key}&text=${param.text}&lang=${param.lang}&format=${param.format}`)
     }
-    return encodeURI(`${api_url}?key=${process.env.API_KEY}&text=${param.text}&lang=${param.lang}`)
+    return encodeURI(`${api_url}?key=${api_key}&text=${param.text}&lang=${param.lang}`)
 }
