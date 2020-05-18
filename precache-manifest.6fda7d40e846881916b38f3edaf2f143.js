cfde9092a24ddca0224706f8ddb02bfec70648b1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-18 08:00:33 +0000

    Deploy foxygen98/2019-2-Track-Frontend-O-Anoshchenko to github.com/foxygen98/2019-2-Track-Frontend-O-Anoshchenko.git:gh-pages

---
+++ precache-manifest.6fda7d40e846881916b38f3edaf2f143.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d39668f4cda867ea400d331e69f3533d",
+    "url": "/2019-2-Track-Frontend-O-Anoshchenko/index.html"
+  },
+  {
+    "revision": "7c0a60cc969cc219b463",
+    "url": "/2019-2-Track-Frontend-O-Anoshchenko/static/css/main.fa44dfbe.chunk.css"
+  },
+  {
+    "revision": "f38afb9680b2214886a3",
+    "url": "/2019-2-Track-Frontend-O-Anoshchenko/static/js/2.9885b3a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2019-2-Track-Frontend-O-Anoshchenko/static/js/2.9885b3a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c0a60cc969cc219b463",
+    "url": "/2019-2-Track-Frontend-O-Anoshchenko/static/js/main.91082d7d.chunk.js"
+  },
+  {
+    "revision": "5ed897ca7416749bedc1",
+    "url": "/2019-2-Track-Frontend-O-Anoshchenko/static/js/runtime-main.0e0b52a9.js"
+  }
+]);
